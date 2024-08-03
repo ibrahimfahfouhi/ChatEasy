d5a3fbbbfdd30340a5ed94ce20da142b5a812bc1
Fahfouhi <fahfouhi@gmail.com>
2024-08-03 15:10:33 +0200

    Adding Frontend implementation for ChatEasy app

--- src/main/java/ma/ibracode/chatapp/Infrastructure/ClientRepository.java
+++ src/main/java/ma/ibracode/chatapp/Infrastructure/ClientRepository.java
@@ -1,6 +1,6 @@
 package ma.ibracode.chatapp.Infrastructure;
 
-import ma.ibracode.chatapp.Domain.entities.User;
+import ma.ibracode.chatapp.Domain.entities.Client;
 import ma.ibracode.chatapp.Domain.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
-    List<User> findAllByStatus(Status status);
+public interface UserRepository extends JpaRepository<Client, String> {
+    List<Client> findAllByStatus(Status status);
 }
